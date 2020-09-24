0575dd8d782c37a57e0c48668375d1cceb854c5c
pierredenelle <pierre.denelle@gmail.com>
2020-09-24 12:33:18 +0100

    env_raster.R created

---
+++ R/env_raster.R
@@ -0,0 +1,12 @@
+
+env_raster <- function(){
+  require(jsonlite)
+  # Return the miscellaneous environmental information
+  return(
+    read_json(paste0(
+      "http://",credentials[[1]], ":",credentials[[2]],
+      "@gift.uni-goettingen.de/api/extended/index.php?query=env_raster")))
+ 
+  # Making internal reference function 
+}
+
