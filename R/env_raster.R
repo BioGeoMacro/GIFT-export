69c190be042e30c024a175ad77551c1b411f2a26
Patrick Weigelt <pweigel@uni-goettingen.de>
2020-09-24 12:46:25 +0200

    raster

--- R/env_raster.R
+++ R/env_raster.R
@@ -1,11 +1,12 @@
 
 env_raster <- function(){
   require(jsonlite)
+  require(dplyr)
   # Return the miscellaneous environmental information
   return(
     read_json(paste0(
       "http://",credentials[[1]], ":",credentials[[2]],
-      "@gift.uni-goettingen.de/api/extended/index.php?query=env_raster")))
+      "@gift.uni-goettingen.de/api/extended/index.php?query=env_raster"), simplifyVector = TRUE))
  
   # Making internal reference function 
 }
