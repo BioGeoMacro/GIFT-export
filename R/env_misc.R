b54592be83147b92157cbc194a665b8b3ad24b39
pierredenelle <pierre.denelle@gmail.com>
2020-09-24 12:11:56 +0100

    env_misc.R corrected

--- R/env_misc.R
+++ R/env_misc.R
@@ -1,11 +1,11 @@
 
 env_misc <- function(){
+  require(jsonlite)
   # Return the miscellaneous environmental information
   return(
-    paste0(
+    read_json(paste0(
       "http://",credentials[[1]], ":",credentials[[2]],
-      "@gift.uni-goettingen.de/api/extended/index.php?query=env_misc&traitid=",
-      misc_variable[i]))
-
+      "@gift.uni-goettingen.de/api/extended/index.php?query=env_misc")))
+  
 }
 
